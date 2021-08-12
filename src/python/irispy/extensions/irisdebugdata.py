352c330460ee7a65833db16de6066c4da7ba177a
Daniel Tar <tardani95@gmail.com>
2021-08-12 10:51:23 +0200

    plotting extended for one-vertex obstacle

--- src/python/irispy/extensions/irisdebugdata.py
+++ src/python/irispy/extensions/irisdebugdata.py
@@ -8,6 +8,7 @@ from .. import drawing
 def iterRegions(self):
     return zip(self.polyhedron_history, self.ellipsoid_history)
 
+
 def animate(self, fig=None, pause=0.5, show=True, repeat_delay=2.0):
     dim = self.bounds.getDimension()
     if dim < 2 or dim > 3:
@@ -23,8 +24,8 @@ def animate(self, fig=None, pause=0.5, show=True, repeat_delay=2.0):
     if bounding_pts.size > 0:
         lb = bounding_pts.min(axis=0)
         ub = bounding_pts.max(axis=0)
-        assert(lb.size == dim)
-        assert(ub.size == dim)
+        assert (lb.size == dim)
+        assert (ub.size == dim)
         width = ub - lb
         ax.set_xlim(lb[0] - 0.1 * width[0], ub[0] + 0.1 * width[0])
         ax.set_ylim(lb[1] - 0.1 * width[1], ub[1] + 0.1 * width[1])
@@ -42,9 +43,15 @@ def animate(self, fig=None, pause=0.5, show=True, repeat_delay=2.0):
         artists.extend(poly.draw(ax))
         artists.extend(ellipsoid.draw(ax))
         for obs in self.getObstacles():
-            artists.extend(drawing.draw_convhull(obs.T, ax, edgecolor='k', facecolor='k', alpha=0.5))
+            if len(obs.T) < 4:
+                if dim == 3:
+                    artists.extend(ax.plot(obs[0][:], obs[1][:], obs[2][:], 'ko-'))
+                else:
+                    artists.extend(ax.plot(obs[0][:], obs[1][:], 'ko-'))
+            else:
+                artists.extend(drawing.draw_convhull(obs.T, ax, edgecolor='k', facecolor='k', alpha=0.5))
         artist_sets.append(tuple(artists))
 
-    ani = animation.ArtistAnimation(fig, artist_sets, interval=pause*1000, repeat_delay=repeat_delay*1000)
+    ani = animation.ArtistAnimation(fig, artist_sets, interval=pause * 1000, repeat_delay=repeat_delay * 1000)
     if show:
         plt.show()
